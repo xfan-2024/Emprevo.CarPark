6796eeceef262b2b38f942a44d8e1d0cdbd6a366
Xiaoyi_Fan <81364043+xfancel@users.noreply.github.com>
2024-08-22 21:36:46 +1000

    Init check in
    Init check in

---
+++ Emprevo.CarPark.Engine/Emprevo.CarPark.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Emprevo.CarPark.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
